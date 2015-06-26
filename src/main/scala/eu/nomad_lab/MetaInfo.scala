59971a64427a17488c86894d0f3cbde27c524eb4
Fawzi Mohamed <fawzi@nomad-sdev1.rz-berlin.mpg.de>
2015-06-26 17:52:37 +0200

    MetaInfo: starting json integration

--- src/main/scala/eu/nomad_lab/MetaInfo.scala
+++ src/main/scala/eu/nomad_lab/MetaInfo.scala
@@ -1,14 +1,51 @@
+package eu.nomad_lab
+import org.json4s._
+import org.json4s.native.JsonMethods._
+
+/** Represents a piece of nomad meta info referring to other meta info by name.
+  *
+  * Can be interpreted within a context [[eu.nomad_lab.MetaInfoEnv]], but matches the
+  * preferred way one describes it in a json file.
+  */
 case class MetaInfoRecord(
-  val name : String,
-  val description : String,
-  val kind_str : String,
-  val super_names : String,
-  val units : Option[String],
-  val dtype_str : Option[String],
-  val repeats : Option[Boolean],
-  val shape : Seq[Int] ) {
-  def
-}
+  val name: String,
+  val kindStr: String,
+  val description: String,
+  val superNames: String,
+  val units: Option[String] = None,
+  val dtypeStr: Option[String] = None,
+  val repeats: Option[Boolean] = None,
+  val shape: Option[Seq[Int]] = None,
+  val otherKeys: Seq[JsonAST.JField] = Seq()) {
+};
+
+/* class MetaInfoRecordSerializer extends CustomSerializer[MetaInfoRecord](format => (
+         {
+           case obj :JObject => {
+             var name: String;
+             val kindStr: String;
+             val description: String;
+             val superNames: String;
+             val units: Option[String] = None;
+             val dtypeStr: Option[String] = None;
+             val repeats: Option[Boolean] = None;
+             val shape: Option[Seq[Int]] = None;
+             val otherKeys: Seq[JsonAST.JField] = Seq();
+             obj.map {
+               case JField("name", value) =>
+                 name = value
+               case JField(key, value) =>
+                 otherKeys += JField(key, value)
+             }
+           }
+         },
+         {
+           case x: =>
+             JObject(JField("start", JInt(BigInt(x.startTime))) ::
+                     JField("end",   JInt(BigInt(x.endTime))) :: Nil)
+         }
+       ))*/
+
 
 class MetaInfoEnv(
   val name: String,
@@ -19,7 +56,7 @@ class MetaInfoEnv(
 
 class MetaInfoDbEnv(
   val name: String,
-  val dbConnection: AkkaRef,
+//  val dbConnection: ActorRef,
   val lazyLoad: Boolean) {
 
 }
