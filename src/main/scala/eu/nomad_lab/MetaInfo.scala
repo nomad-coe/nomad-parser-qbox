098e8799d1d192c76e8a76147898c2215e319e59
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2015-07-02 00:24:11 +0200

    First real specs2 tests for meta info json read

--- src/main/scala/eu/nomad_lab/MetaInfo.scala
+++ src/main/scala/eu/nomad_lab/MetaInfo.scala
@@ -1,51 +1,97 @@
 package eu.nomad_lab
 import org.json4s._
 import org.json4s.native.JsonMethods._
+import org.json4s.DefaultFormats
 
 /** Represents a piece of nomad meta info referring to other meta info by name.
   *
   * Can be interpreted within a context [[eu.nomad_lab.MetaInfoEnv]], but matches the
   * preferred way one describes it in a json file.
   */
-case class MetaInfoRecord(
+class MetaInfoRecord(
   val name: String,
   val kindStr: String,
   val description: String,
-  val superNames: String,
+  val superNames: Seq[String],
   val units: Option[String] = None,
   val dtypeStr: Option[String] = None,
   val repeats: Option[Boolean] = None,
   val shape: Option[Seq[Int]] = None,
-  val otherKeys: Seq[JsonAST.JField] = Seq()) {
-};
+  val otherKeys: Seq[JField] = Seq()) {
+}
 
-/* class MetaInfoRecordSerializer extends CustomSerializer[MetaInfoRecord](format => (
+object MetaInfoRecord {
+  final val dtypes = Seq("f32", "i32", "u32", "f64", "i64", "u64", "b", "c", "B", "C")
+}
+
+case class MissingFieldError(val fieldName: String, val context: String) extends Throwable {
+  override def toString(): String = {
+    "missing field " ++ fieldName ++ " in " ++ context
+  }
+}
+
+case class InvalidValueError(val fieldName: String, val context: String, value: String, expected :String) extends Throwable {
+  override def toString(): String = {
+    "invalid value for field " ++ fieldName ++ " in " ++ context ++ " expected (" ++ expected ++ ") but got " ++ value
+  }
+}
+
+class MetaInfoRecordSerializer extends CustomSerializer[MetaInfoRecord](format => (
          {
-           case obj :JObject => {
-             var name: String;
-             val kindStr: String;
-             val description: String;
-             val superNames: String;
-             val units: Option[String] = None;
-             val dtypeStr: Option[String] = None;
-             val repeats: Option[Boolean] = None;
-             val shape: Option[Seq[Int]] = None;
-             val otherKeys: Seq[JsonAST.JField] = Seq();
-             obj.map {
+           case JObject(obj) => {
+             implicit val formats = DefaultFormats;
+             var name: String = "";
+             var kindStr: String = "DocumentContentType";
+             var description: String = "";
+             var superNames: Seq[String] = Seq();
+             var units: Option[String] = None;
+             var dtypeStr: Option[String] = None;
+             var repeats: Option[Boolean] = None;
+             var shape: Option[Seq[Int]] = None;
+             var otherKeys: Seq[JsonAST.JField] = Seq();
+             obj foreach {
                case JField("name", value) =>
-                 name = value
+                 name = value.extract[String]
+               case JField("kindStr", value) =>
+                 kindStr = value.extract[String]
+               case JField("description", value) =>
+                 description = value.extract[String]
+               case JField("superNames", value) =>
+                 superNames = value.extract[Seq[String]]
+               case JField("units", value) =>
+                 units = value.extract[Option[String]]
+               case JField("dtypeStr", value) =>
+                 dtypeStr = value.extract[Option[String]]
+               case JField("repeats", value) =>
+                 repeats = value.extract[Option[Boolean]]
+               case JField("shape", value) =>
+                 shape = value.extract[Option[Seq[Int]]]
                case JField(key, value) =>
-                 otherKeys += JField(key, value)
+                 otherKeys = JField(key, value) +: otherKeys
              }
+             if (name.isEmpty) throw new MissingFieldError("name", "NomadMetaInfo")
+             if (description.isEmpty) throw new MissingFieldError("description", "NomadMetaInfo")
+             if (!dtypeStr.isEmpty && !(MetaInfoRecord.dtypes contains dtypeStr.get))
+               throw new InvalidValueError("dtypeStr", "NomadMetaInfo", dtypeStr.get, MetaInfoRecord.dtypes.foldLeft(""){ _ + " " + _ })
+             new MetaInfoRecord(name, kindStr, description, superNames, units,
+               dtypeStr, repeats, shape, otherKeys)
            }
          },
          {
-           case x: =>
-             JObject(JField("start", JInt(BigInt(x.startTime))) ::
-                     JField("end",   JInt(BigInt(x.endTime))) :: Nil)
+           case x: MetaInfoRecord => {
+             import org.json4s.JsonDSL._;
+             ("name", x.name) ~
+             ("kindStr", (if (x.kindStr == "DocumentContentType") None else Some(x.kindStr)))~
+             ("description", x.description)~
+             ("superNames", x.superNames)~
+             ("units", x.units)~
+             ("dtypeStr", x.dtypeStr)~
+             ("repeats", x.repeats)~
+             ("shape", x.shape) ++
+             x.otherKeys
+           }
          }
-       ))*/
-
+       ))
 
 class MetaInfoEnv(
   val name: String,
---
+++ src/test/scala/eu/nomad_lab/MetaInfoSpec.scala
@@ -0,0 +1,123 @@
+package eu.nomad_lab
+
+import org.specs2._
+import org.json4s.DefaultFormats
+//import eu.nomad_lab.MetaInfoRecord
+import org.json4s.native.Serialization
+import org.json4s.native.Serialization.{read, write}
+import org.json4s._
+import org.json4s.native.JsonMethods._
+
+class MetaInfoJsonSpec extends mutable.Specification {
+val pippo = """
+
+MetaInfo json read
+ read basic json working            $jsonRead
+ read extended json working         $ok
+ read write read same               $ok
+ write read write same              $ok
+"""
+
+implicit val formats = DefaultFormats + new eu.nomad_lab.MetaInfoRecordSerializer
+
+  "jsonExtract basic" >> {
+    val jVal = parse("""
+    {
+        "name": "TestProperty1",
+        "description": "a meta info property to test serialization to json",
+        "superNames": []
+    }""")
+    val mRecord = jVal.extract[MetaInfoRecord]
+    mRecord.name must_== "TestProperty1"
+    mRecord.description must_== "a meta info property to test serialization to json"
+    mRecord.superNames must beEmpty
+    mRecord.kindStr must_== "DocumentContentType"
+    mRecord.units must beNone
+    mRecord.repeats must beNone
+    mRecord.shape must beNone
+    mRecord.otherKeys must beEmpty
+  }
+
+  "jsonRead basic" >> {
+    val mRecord = read[MetaInfoRecord]("""
+    {
+        "name": "TestProperty1",
+        "description": "a meta info property to test serialization to json",
+        "superNames": []
+    }""")
+    mRecord.name must_== "TestProperty1"
+    mRecord.description must_== "a meta info property to test serialization to json"
+    mRecord.superNames must beEmpty
+    mRecord.kindStr must_== "DocumentContentType"
+    mRecord.units must beNone
+    mRecord.repeats must beNone
+    mRecord.shape must beNone
+    mRecord.dtypeStr must beNone
+    mRecord.otherKeys must beEmpty
+  }
+
+  "jsonRead extended" >> {
+    val mRecord = read[MetaInfoRecord]("""
+    {
+        "name": "TestProperty2",
+        "kindStr": "DocumentContentType"
+        "description": "a meta info property to test serialization to json",
+        "superNames": [],
+        "units": "pippo",
+        "dtypeStr": "f64",
+        "repeats": false,
+        "shape": []
+    }""")
+    mRecord.name must_== "TestProperty2"
+    mRecord.description must_== "a meta info property to test serialization to json"
+    mRecord.superNames must beEmpty
+    mRecord.kindStr must_== "DocumentContentType"
+    mRecord.units must_== Some("pippo")
+    mRecord.repeats must_== Some(false)
+    mRecord.shape must_== Some(Seq())
+    mRecord.otherKeys must beEmpty
+  }
+
+  "jsonRead null" >> {
+    val mRecord = read[MetaInfoRecord]("""
+    {
+        "name": "TestProperty3",
+        "kindStr": "DocumentContentType"
+        "description": "a meta info property to test serialization to json",
+        "superNames": ["TestProperty"],
+        "units": null,
+        "dtypeStr": null,
+        "repeats": null,
+        "shape": null
+    }""")
+    mRecord.name must_== "TestProperty3"
+    mRecord.description must_== "a meta info property to test serialization to json"
+    mRecord.superNames must_== Seq("TestProperty")
+    mRecord.kindStr must_== "DocumentContentType"
+    mRecord.units must beNone
+    mRecord.repeats must beNone
+    mRecord.shape must beNone
+    mRecord.otherKeys must beEmpty
+  }
+
+  "jsonRead extra" >> {
+    val mRecord = read[MetaInfoRecord]("""
+    {
+        "name": "TestProperty4",
+        "kindStr": "DocumentContentType"
+        "description": "a meta info property to test serialization to json",
+        "superNames": ["TestProperty"],
+        "extra": "nr1",
+        "tt": 4
+    }""")
+    mRecord.name must_== "TestProperty4"
+    mRecord.description must_== "a meta info property to test serialization to json"
+    mRecord.superNames must_== Seq("TestProperty")
+    mRecord.kindStr must_== "DocumentContentType"
+    mRecord.units must beNone
+    mRecord.repeats must beNone
+    mRecord.shape must beNone
+    mRecord.otherKeys must_==  JField("tt", JInt(4)) :: JField("extra", JString("nr1")) :: Nil
+  }
+
+}
