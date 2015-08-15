56aae02d6dc7f49aabf7930fadce1cbdb8d3998f
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2015-08-16 00:30:38 +0200

    JSonUtils: produce UTF_8 string by default, avoid import _

--- src/main/scala/eu/nomad_lab/JsonUtils.scala
+++ src/main/scala/eu/nomad_lab/JsonUtils.scala
@@ -1,90 +1,104 @@
 package eu.nomad_lab;
 
-import org.json4s._
-import org.json4s.native.JsonMethods._
-import org.json4s.DefaultFormats
+import java.nio.charset.StandardCharsets
+import org.json4s.{JNothing, JNull, JBool, JDouble, JDecimal, JInt, JString, JArray, JObject, JValue, JField}
+import scala.collection.mutable.ListBuffer
 
+/** methods to handle json, in particular serialize it in a predictable way directly to UTF_8
+  *
+  * first implementation, could be improved, but worth doing only with real benchmarks
+  */
 object JsonUtils {
 
-  private final val newlineRe = "\n".r;
   /** Adds the given amount of extra indent at the beginning of each line
     */
-  def extraIndenter(dumper: String => Unit, extraIndent: Int): String => Unit = {
-    val indent: String = "\n" + (" " * extraIndent);
-    { s: String =>
+  def extraIndenter(dumper: Array[Byte] => Unit, extraIndent: Int): Array[Byte] => Unit = {
+    val indent: Array[Byte] = ("\n" + (" " * extraIndent)).getBytes(StandardCharsets.UTF_8)
+
+    { s: Array[Byte] =>
       // dumper(s.replace("\n", indent))
       var i0 = 0
-      for (m <- newlineRe.findAllMatchIn(s)) {
-        dumper(s.slice(i0, m.start))
+      var i1 = s.indexOf('\n'.toByte, i0)
+      while (i1 >= 0) {
+        dumper(s.slice(i0, i1))
         dumper(indent)
-        i0 = m.end
+        i0 = i1 + 1
+        i1 = s.indexOf('\n'.toByte, i0)
       }
       dumper(s.slice(i0, s.length))
     }
   }
 
-  private final val toEscapeRe = "\\\\|\"".r;
-  /** Dumps the given string escaping \ and "
+  /** Dumps the given UTF_8 encoded string escaping \ and "
     */
-  def dumpString(dumper: String => Unit, s: String): Unit = {
-    dumper("\"")
+  def dumpStringArray(dumper: Array[Byte] => Unit, s: Array[Byte]): Unit = {
+    dumper(Array('"'.toByte))
     var i0 = 0
-    for (m <- toEscapeRe.findAllMatchIn(s)) {
-      dumper(s.slice(i0, m.start))
-      dumper("\\")
-      dumper(m.matched)
-      i0 = m.end
+    var j = i0
+    while (j < s.length) {
+      val c = s(j)
+      if (c == '\\'.toByte || c == '"'.toByte) {
+        dumper(s.slice(i0, j))
+        dumper(Array('\\'.toByte, c))
+        i0 = j + 1
+      }
+      j += 1
     }
     dumper(s.slice(i0, s.length))
-    dumper("\"")
+    dumper(Array('"'.toByte))
   }
+
+  /** Dumps the given string escaping \ and "
+    */
+  def dumpString(dumper: Array[Byte] => Unit, s: String): Unit =
+    dumpStringArray(dumper, s.getBytes(StandardCharsets.UTF_8))
 
   /** Dumps an compact ordered json
     * 
     * Object keys are alphabetically ordered
     */
-  def jsonCompactDump(dumper: String => Unit, obj: JValue): Unit = {
+  def jsonCompactDump(dumper: Array[Byte] => Unit, obj: JValue): Unit = {
     obj match {
       case JString(s) =>
         dumpString(dumper, s)
       case JNothing =>
         ()
       case JDouble(num) =>
-        dumper(num.toString)
+        dumper(num.toString.getBytes(StandardCharsets.UTF_8))
       case JDecimal(num) =>
-        dumper(num.toString)
+        dumper(num.toString.getBytes(StandardCharsets.UTF_8))
       case JInt(num) =>
-        dumper(num.toString)
+        dumper(num.toString.getBytes(StandardCharsets.UTF_8))
       case JBool(value) =>
         if (value)
-          dumper("true")
+          dumper("true".getBytes(StandardCharsets.UTF_8))
         else
-          dumper("false")
+          dumper("false".getBytes(StandardCharsets.UTF_8))
       case JNull =>
-        dumper("null")
+        dumper("null".getBytes(StandardCharsets.UTF_8))
       case JObject(obj) =>
-        dumper("{")
+        dumper(Array('{'.toByte))
         val iter = obj.toArray.sortBy(_._1).iterator
         while (iter.hasNext) {
           val (key, value) = iter.next
           if (value != JNothing) {
-            dumpString(dumper,key)
-            dumper(":")
-            jsonCompactDump(dumper, value)
+            dumpString(dumper, key)
+            dumper(Array(':'.toByte))
+            jsonCompactDump(dumper, value) // recursive call
             while (iter.hasNext) {
               val (key2, value2) = iter.next
               if (value2 != JNothing) {
-                dumper(",")
+                dumper(Array(','.toByte))
                 dumpString(dumper, key2)
-                dumper(":")
-                jsonCompactDump(dumper, value2)
+                dumper(Array(':'.toByte))
+                jsonCompactDump(dumper, value2) // recursive call
               }
             }
           }
         }
-        dumper("}")
+        dumper(Array('}'.toByte))
       case JArray(arr) =>
-        dumper("[");
+        dumper(Array('['.toByte));
         val iter = arr.iterator
         while (iter.hasNext) {
           val value = iter.next
@@ -93,23 +107,40 @@ object JsonUtils {
             while (iter.hasNext) {
               val value2 = iter.next
               if (value2 != JNothing) {
-                dumper(",")
-                jsonCompactDump(dumper, value2)
+                dumper(Array(','.toByte))
+                jsonCompactDump(dumper, value2) // recursive call
               }
             }
           }
         }
-        dumper("]")
+        dumper(Array(']'.toByte))
     }
   }
 
-  /** returns a string with a compact sorted json representation
+  /** returns a compact sorted json representation
     */
-  def jsonCompactStr(value: JValue): String = {
-    val sb = new StringBuilder();
-    jsonCompactDump(sb.append(_), value)
-    sb.result()
+  def jsonCompact(value: JValue): Array[Byte] = {
+    val pieces = new ListBuffer[Array[Byte]]()
+    var totLen = 0
+    jsonCompactDump({s: Array[Byte] =>
+      pieces.append(s)
+      totLen += s.length
+    }, value)
+    val res = new Array[Byte](totLen)
+    var i0 = 0;
+    for (p <- pieces) {
+      p.copyToArray(res, i0, i0 + p.length)
+      i0 += p.length
+    }
+    res
   }
+
+  /** returns a string with a compact sorted json representation
+    *
+    * this isjust for convenience, try to avoid its use
+    */
+  def jsonCompactStr(value: JValue): String =
+    new String(jsonCompact(value), StandardCharsets.UTF_8)
 
   /** calculates a measure of the complexity (size) of the json
     */
