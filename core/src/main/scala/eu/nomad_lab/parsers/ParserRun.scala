bbb1aa9958ff069666d362ec25481083f76813c8
ankit kariryaa <kariryaa@fhi-berlin.mpg.de>
2016-02-10 15:08:47 +0100

    Add method to parse data using a given parser

---
+++ core/src/main/scala/eu/nomad_lab/parsers/ParserRun.scala
@@ -0,0 +1,43 @@
+package eu.nomad_lab.parsers
+
+import java.nio.file.Paths
+import eu.nomad_lab.parsers
+import scala.collection._
+import scala.collection.mutable.ListBuffer
+import com.typesafe.scalalogging.StrictLogging
+
+object ParserRun {
+
+  /* Parse the given file path with the given parser and options
+  *
+  * */
+  def parse(Parser:SimpleExternalParserGenerator,pPath:String,opt:String) =
+  {
+    object BackendType extends Enumeration {
+      type Enum = Value
+      val JsonWriter, JsonEventEmitter, Netcdf = Value
+    }
+    val p = Paths.get(pPath).toAbsolutePath
+    val path = p.toString
+    val uri = p.toUri.toString
+    val stdOut = new java.io.OutputStreamWriter(System.out)
+    val optimizer = Parser.optimizedParser(Seq())
+    var backendType: BackendType.Enum = BackendType.JsonEventEmitter
+    opt match {
+      case "json" =>
+      backendType = BackendType.JsonWriter
+      case "hdf5" | "netcdf" =>
+      backendType = BackendType.Netcdf
+      case _ =>
+      backendType = BackendType.JsonEventEmitter
+    }
+    val extBackend: parsers.ParserBackendExternal = backendType match {
+      case BackendType.JsonWriter =>
+        new parsers.JsonWriterBackend(optimizer.parseableMetaInfo, stdOut)
+      case BackendType.Netcdf => null
+      case BackendType.JsonEventEmitter =>
+        new parsers.JsonParseEventsWriterBackend(optimizer.parseableMetaInfo, stdOut)
+    }
+    optimizer.parseExternal(uri,path,extBackend,Parser.name)
+  }
+}
---
+++ tool/src/test/scala/eu/nomad_lab/parsers/CLIToolSpec.scala
@@ -0,0 +1,15 @@
+package eu.nomad_lab.parsers
+
+import com.typesafe.scalalogging.StrictLogging
+import eu.nomad_lab.{CLITool, parsers, DefaultPythonInterpreter}
+import org.{json4s => jn}
+import org.specs2.mutable.Specification
+import java.nio.charset.StandardCharsets
+
+import scala.collection._
+
+/** Specification (fixed tests) for MetaInfo serialization
+  */
+class CLIToolSpec extends Specification with StrictLogging {
+
+}
